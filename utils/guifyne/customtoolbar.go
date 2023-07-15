ccec75acfc9de735c6253ffb2810dab4b6539e0f
Faysal AL-Banna <degreane@gmail.com>
2023-07-15 17:25:30 +0300

    Added Commitments

--- utils/guifyne/customtoolbar.go
+++ utils/guifyne/customtoolbar.go
@@ -3,11 +3,17 @@
 package guifyne
 
 import (
+	"errors"
+	"log"
+	"regexp"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
 )
 
+// button PsyButtonToolbar
 type PsyButtonToolbar interface {
 	widget.ToolbarItem
 	SetHighPriority()
@@ -50,6 +56,9 @@ func (l *customToolbarItem) Settext(t string) {
 	l.Refresh()
 }
 
+// End of PsyButtonToolbar
+
+// Label PsyLabelToolbar
 type PsyLabelToolbar interface {
 	widget.ToolbarItem
 	SetHighPriority()
@@ -83,4 +92,103 @@ func (l *customLabelToolbarItem) Settext(str string) {
 }
 func (l *customLabelToolbarItem) SetBinding(b binding.String) {
 	l.Bind(b)
+} // End of PsyLabelToolbar
+
+// CheckBox PsyCheckboxToolbar
+type PsyCheckboxToolbar interface {
+	widget.ToolbarItem
+	Settext(str string)
+	SetBinding(bind binding.Bool)
 }
+type customCheckToolbarItem struct {
+	*widget.Check
+}
+
+func NewCustomToolbarCheckItem(str string, fn func(bool)) PsyCheckboxToolbar {
+	l := widget.NewCheck(str, fn)
+	return &customCheckToolbarItem{l}
+}
+func (l *customCheckToolbarItem) SetBinding(bind binding.Bool) {
+	l.Bind(bind)
+}
+func (l *customCheckToolbarItem) Settext(str string) {
+	l.Text = str
+}
+func (l *customCheckToolbarItem) ToolbarObject() fyne.CanvasObject {
+	return l.Check
+}
+
+// End of PsyCheckboxToolbar
+
+// Entry PsyEntryToolbar
+type PsyEntryToolbar interface {
+	widget.ToolbarItem
+	SetBinding(bind binding.String)
+	OnChange(fn func(str string))
+	Resize(size fyne.Size)
+	Filter(re string)
+	SetText(txt string)
+	Get() *widget.Entry
+}
+type customEntryToolbarItem struct {
+	*widget.Entry
+}
+
+func (l *customEntryToolbarItem) ToolbarObject() fyne.CanvasObject {
+	return container.NewMax(l.Entry)
+
+}
+func NewCustomToolbarEntryItem() PsyEntryToolbar {
+	e := widget.NewEntry()
+	return &customEntryToolbarItem{
+		e,
+	}
+}
+func (l *customEntryToolbarItem) SetBinding(bind binding.String) {
+	l.Entry.Bind(bind)
+}
+func (l *customEntryToolbarItem) OnChange(fn func(str string)) {
+	l.Entry.OnChanged = fn
+}
+func (l *customEntryToolbarItem) Resize(size fyne.Size) {
+	oSize := l.Entry.Size()
+	log.Println("<Old Size ", oSize, ">")
+	l.Entry.Resize(size)
+	l.Entry.Refresh()
+	log.Println("New Size <", l.Entry.Size(), ">")
+	/*if size.Width < 60 {
+		nSize := fyne.NewSize(60, oSize.Height)
+		l.Entry.Resize(nSize)
+	} else {
+		nSize := fyne.NewSize(size.Width, oSize.Height)
+		l.Entry.Resize(nSize)
+		log.Println(l.Entry.Size())
+	}*/
+}
+func (l *customEntryToolbarItem) Filter(re string) {
+	l.Entry.Validator = fyne.StringValidator(
+		func(s string) error {
+			reg, err := regexp.Compile(re)
+			if err != nil {
+				log.Panicf("%+v", err)
+			}
+			l.Entry.SetValidationError(errors.New("Invalid Entry"))
+			log.Printf("<String %s>", s)
+			ok := reg.MatchString(s)
+			if !ok {
+				return errors.New("Invalid Entry")
+			} else {
+				return nil
+			}
+
+		},
+	)
+}
+func (l *customEntryToolbarItem) SetText(txt string) {
+	l.Entry.SetText(txt)
+}
+func (l *customEntryToolbarItem) Get() *widget.Entry {
+	return l.Entry
+}
+
+// End of PsyEntryToolbar
--- utils/guifyne/new_node_window.go
+++ utils/guifyne/new_node_window.go
@@ -2,14 +2,47 @@ package guifyne
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
 )
 
 var (
 	new_node_window        fyne.Window
 	new_node_window_IsOpen binding.Bool = binding.NewBool()
 )
+var (
+	_new_node_auto             binding.Bool   = binding.NewBool()
+	_new_node_auto_count       binding.Int    = binding.NewInt()
+	_new_node_auto_countString binding.String = binding.NewSprintf("%d", _new_node_auto_count)
+)
+
+func init_new_node_window_toolbar() fyne.CanvasObject {
+	_toolbar_auto_nodes_count := NewCustomToolbarEntryItem()
+	_new_node_auto_count.Set(1)
+	_toolbar_auto_nodes_count.SetBinding(_new_node_auto_countString)
+	_toolbar_auto_nodes_count.Filter("^[0-9]+$")
+	_toolbar_auto_nodes_count.OnChange(func(str string) {
+		_toolbar_auto_nodes_count.Resize(fyne.NewSize(120, 34))
+		_toolbar_auto_nodes_count.Get().Validate()
+	})
+	_toolbar_auto_nodes_count.SetText("1")
+	_toolbar_auto_nodes := NewCustomToolbarCheckItem("Auto", func(b bool) {
+		if b {
+
+		}
+	})
+	_toolbar_auto_nodes.SetBinding(_new_node_auto)
+	_toolbar := widget.NewToolbar(
+		_toolbar_auto_nodes,
+		widget.NewToolbarSeparator(),
+		NewCustomToolBarLabelItem("<Count>:"),
+		_toolbar_auto_nodes_count,
+	)
+	_new_node_auto_count.Set(12)
+	return _toolbar
+}
 
 func init_new_node_window() {
 	if ok, _ := new_node_window_IsOpen.Get(); ok {
@@ -17,10 +50,16 @@ func init_new_node_window() {
 		dlg.Show()
 		new_node_window.RequestFocus()
 	} else {
+
 		new_node_window = gui_Application.NewWindow("New Node")
 		new_node_window_IsOpen.Set(true)
 		new_node_window.Resize(fyne.NewSize(600, 400))
 		new_node_window.CenterOnScreen()
+		new_node_window.SetContent(
+			container.NewVBox(
+				init_new_node_window_toolbar(),
+			),
+		)
 		new_node_window.Show()
 		new_node_window.SetOnClosed(func() {
 			new_node_window_IsOpen.Set(false)
